dfcbc6718d78cb99ebc6ab0154641d496968e1e5
Leocaliban <leocaliban@gmail.com>
2019-10-01 17:59:31 -0300

    Saving a new Department

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,18 +3,25 @@ package gui;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import db.DBException;
+import gui.utils.Alerts;
 import gui.utils.Constraints;
+import gui.utils.Utils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
 
 	private Department entity;
+
+	private DepartmentService service;
 
 	@FXML
 	private TextField inputId;
@@ -33,10 +40,31 @@ public class DepartmentFormController implements Initializable {
 
 	@FXML
 	public void onButtonSaveAction(ActionEvent event) {
+		if (entity == null) {
+			throw new IllegalStateException("Entity was null");
+		}
+		if (service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		try {
+			entity = getFormData();
+			service.saveOrUpdate(entity);
+			Utils.currentStage(event).close();
+		} catch (DBException e) {
+			Alerts.showAlert("Error saving object", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+	private Department getFormData() {
+		Department obj = new Department();
+		obj.setId(Utils.tryParseToInt(inputId.getText()));
+		obj.setName(inputName.getText());
+		return obj;
 	}
 
 	@FXML
 	public void onButtonCancelAction(ActionEvent event) {
+		Utils.currentStage(event).close();
 	}
 
 	@Override
@@ -60,6 +88,10 @@ public class DepartmentFormController implements Initializable {
 
 	public void setDepartment(Department entity) {
 		this.entity = entity;
+	}
+
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 
 }
